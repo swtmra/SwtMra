ab4dfd46f7cb5181a9650ea54189e9fc54439487
Hossam <swtmra@gmail.com>
2022-01-16 19:50:48 +0100

    aa

--- src/main/java/interfaces/IMovieDatabase.java
+++ src/main/java/interfaces/IMovieDatabase.java
@@ -1,9 +1,15 @@
 package interfaces;
 
+import dbadapter.MovieDatabase;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface IMovieDatabase {
     boolean addingMovie(String title, String director, String actors, String publishingDate);
     void setAVG(double avgValue, int movieID);
     boolean movieExists(String title, String director, String publishingDate) throws ClassNotFoundException;
+    List<MovieDatabase> get_Movies() throws SQLException;
 
     //TODO there'S no basis how to implement this method (it was wrongly built all along)
     //boolean movieInUsersList(int movieID, User registeredUser);
